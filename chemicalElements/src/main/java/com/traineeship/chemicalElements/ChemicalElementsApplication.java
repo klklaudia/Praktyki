2d1007151dfcb9bb19aa089da342ebf7ec04c4b6
klklaudia <klaudiastec44@gmail.com>
2025-09-18 10:49:09 +0200

    Spring App initialization

---
+++ chemicalElements/src/main/java/com/traineeship/chemicalElements/ChemicalElementsApplication.java
@@ -0,0 +1,13 @@
+package com.traineeship.chemicalElements;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChemicalElementsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChemicalElementsApplication.class, args);
+	}
+
+}
---
+++ chemicalElements/src/test/java/com/traineeship/chemicalElements/ChemicalElementsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.traineeship.chemicalElements;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChemicalElementsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
